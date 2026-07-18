3696eae4fae8bd41590915922b4030f344b048ac
Carlos Mateus <goncalves.mateus@escolar.ifrn.edu.br>
2026-07-18 17:20:51 -0300

    refactor: responsivity

--- portifolio/src/componentes/projects/cardproject/cardproject.jsx
+++ portifolio/src/componentes/projects/cardproject/cardproject.jsx
@@ -4,7 +4,7 @@ export default function CardProject({ title, link, img, description, button}) {
     <div className='my-card d-flex flex-column justify-content-start align-items-center rounded-3'>
         <div className='card-content '>
             <img src={img} alt={title} />
-            <div className='px-2 mt-2 mt-lg-3 '>
+            <div className='card-body px-2 mt-2 mt-lg-3 '>
               <h3 className='title text-start  '>{title}</h3>
               <p className='description text-start mt-0 mt-lg-3'>{description}</p>
               <a className='btn' href={link} target="_blank" rel="noreferrer">{button}  </a>
