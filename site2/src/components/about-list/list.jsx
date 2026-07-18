ea526c056c7477dbb952d0da265b8077dec64480
Carlos Mateus <goncalves.mateus@escolar.ifrn.edu.br>
2026-07-18 17:25:20 -0300

    delete: site2 and site5

--- site2/src/components/about-list/list.jsx
+++
@@ -1,9 +0,0 @@
-import './list.css'
-
-export default function List({title}) {
-    return (
-        <div className='d-flex flex-row'>
-            <i className="bi bi-check-circle"></i>
-            <p className='text-secondary ms-2'>{title}</p>
-        </div>)
-}
--- site2/src/components/button/button.jsx
+++
@@ -1,9 +0,0 @@
-import './button.css'
-
-export default function Button({text , link}) {
-    return (            
-        <a className="custom-button text-white py-2 px-4" href={link} >
-            <strong>{text}</strong>
-        </a>
-    )
-}
